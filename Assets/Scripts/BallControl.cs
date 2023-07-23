25a38f00f42f1b0db2c30f97250075a7426a0fe7
adv-p-n <advaithpnair170302@gmail.com>
2023-07-23 21:29:40 +0530

    Ball Movement Updated

--- Assets/Scripts/BallControl.cs
+++ Assets/Scripts/BallControl.cs
@@ -3,73 +3,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class BallControl : MonoBehaviour
 {
-    SpriteRenderer mySpriteRenderer;
     Rigidbody2D myRigidbody;
-    Vector2 position = new Vector2(0,0);
-    Vector2 moveInput= new Vector2(0,0);
-    [SerializeField] float moveSpeed=0.5f;
-    float xRange=2.6f;
-    float yRange = 4f;
+    Vector2 startPosition = new Vector2(0,0);
+    Vector2 endPosition= new Vector2(0,0);
+    [SerializeField] float moveSpeed=10f;
+    [SerializeField] bool hasClicked=false;
     [SerializeField] bool isDragging=false;
     private void Awake()
     {
-        mySpriteRenderer = GetComponent<SpriteRenderer>();
         myRigidbody= GetComponent<Rigidbody2D>();
     }
-    void Start()
-    {
-        position= transform.position;
-        Debug.Log(position);
-    }
 
-    // Update is called once per frame
     void Update()
     {
-        MoveBall();
-        BallPhysics();
+      if(Input.GetMouseButtonDown(0))
+        {
+            MouseDown();
+        }
+      if(Input.GetMouseButton(0) && hasClicked )
+        {
+            MouseDrag();
+        }
+      if(Input.GetMouseButtonUp(0) && isDragging) 
+        {
+            MouseUp();
+        }
     }
 
-    private void MoveBall()
+     void MouseUp()
     {
-        position.x = Mathf.Clamp(position.x +( moveInput.x*moveSpeed), -xRange, xRange);
-        position.y = Mathf.Clamp(position.y +( moveInput.y*moveSpeed), -yRange, yRange);
-        transform.position=new Vector3(position.x,position.y,0);
-        
+        float ballVelocityX = (startPosition.x - endPosition.x);
+        float ballVelocityY = (startPosition.y - endPosition.y);
+        Vector2 velocity = new Vector2(ballVelocityX, ballVelocityY).normalized;
+        myRigidbody.velocity=velocity*moveSpeed;
+        isDragging = false;
+        hasClicked = false;
     }
 
-    void OnMove(InputValue value)
+    void MouseDrag()
     {
-        moveInput = value.Get<Vector2>();
-        Debug.Log(position);
-    }
-     void BallPhysics()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            mySpriteRenderer.color = Color.red;
-            isDragging = true;
-        }
-        else if(Input.GetMouseButtonUp(0)) 
-        {
-            mySpriteRenderer.color = Color.white;
-            isDragging = false;
-        }
-        if (isDragging) 
-        {
-            mySpriteRenderer.color = Color.blue;
-            transform.position = GetMouseWorldPosition();
-            Debug.Log(GetMouseWorldPosition());
-        }
-        
+        endPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        isDragging = true;
     }
 
-     Vector3 GetMouseWorldPosition()
+    void MouseDown()
     {
-        Vector3 mousePosition = Input.mousePosition ;
-        mousePosition.z = Camera.main.nearClipPlane;
-        return Camera.main.ScreenToWorldPoint(mousePosition);
+        startPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        hasClicked = true;
+    }
+
+     void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag =="Ground")
+        {
+            myRigidbody.velocity = new Vector2(0, 0);
+        }
     }
 }
